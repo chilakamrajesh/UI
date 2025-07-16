4dcca4d0b3675350ad9f3e6a06e2398f5b76b1cb
chilakamrajesh <rajeshreddychilakam@gmail.com>
2025-07-16 09:46:29 +0530

    hi

---
+++ july prostack/JS IT/forof.js
@@ -0,0 +1,4 @@
+let emp_IDs=[101,102,103,104]
+for(let eid of emp_IDs){
+    console.log()
+}
---
+++ july prostack/JS IT/four.js
@@ -0,0 +1,8 @@
+//how to verify JS array is empty or not
+let emp_IDs=[101,102,103]
+if (emp_IDs.length>0){
+    console.log('not empty array')
+}
+else{
+    console.log('empty array')
+} 
